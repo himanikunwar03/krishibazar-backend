95225f7d99a274eed0bbd0ad98ca67cf17ad0253
himanikunwar098@gmail.com <himanikunwar098@gmail.com>
2026-06-09 16:34:13 +0545

    project backend setup

---
+++ src/configs/constant.ts
@@ -0,0 +1,16 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT = process.env.PORT || 8088;
+export const MOCK_DB = process.env.MOCK_DB || "mock";
+export const MONGODB_URI = process.env.MONGODB_URI
+
+    || "mongodb://localhost:27017/class36b";
+export const SECRET_KEY = process.env.SECRET_key
+    || "meroscretjwtkey";
+// same as
+// export{
+// PORt
+//MOCK_DB
+// }
---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,27 @@
+
+    //1.create new schema 
+    // export const LoginUserDto = z.object({
+    // email : z.email(),
+    // password: z.string().min)(6),
+    //})
+    //2. use pick to create 
+
+
+import { UserSchema } from "../types/user.type";
+import { z } from "zod";
+// what client sends and need to validate before processing
+export const CreateUserDto = UserSchema.pick({
+    firstName: true,
+    lastName: true,
+    email: true,
+    username: true,
+    password: true,
+});
+
+export type CreateUserDto = z.infer<typeof CreateUserDto>;
+
+export const LoginUserDto =UserSchema.pick({
+    email:true,
+    password:true,
+})
+export type LoginUserDto = z.infer<typeof LoginUserDto>;
